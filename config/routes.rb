9708d87035e1db5bbf043989c778de6e83648a18
Gusta <lgx.0097@gmail.com>
2020-01-05 12:09:38 -0300

    send messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :contacts, only: [:index, :new, :create, :destroy]
-  resources :chats, only: [:index, :show]
+  resources :chats, only: [:index, :show] do
+    resources :messages, only: :create, defaults: { format: :js }
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
